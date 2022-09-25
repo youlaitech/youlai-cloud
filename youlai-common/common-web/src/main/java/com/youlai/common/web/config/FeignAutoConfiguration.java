34e6139963a60f28fe2416a286e6765f2d262401
horizons <1490493387@qq.com>
2022-09-25 11:37:51 +0800

    fix(FeignAutoConfiguration): 添加序列化配置解决Feign调用反序列化报错 missing type id property ‘@class 问题

---
+++ youlai-common/common-web/src/main/java/com/youlai/common/web/config/FeignAutoConfiguration.java
@@ -0,0 +1,111 @@
+
+package com.youlai.common.web.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
+import org.springframework.cloud.openfeign.support.SpringDecoder;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+
+/**
+ * Feign相关配置类
+ *
+ * @author Gadfly
+ * @since 2021-08-06 9:47
+ */
+
+@AutoConfiguration
+public class FeignAutoConfiguration {
+    /**
+     * 让DispatcherServlet向子线程传递RequestContext
+     *
+     * @param servlet servlet
+     * @return 注册bean
+     */
+    @Bean
+    public ServletRegistrationBean<DispatcherServlet> dispatcherRegistration(DispatcherServlet servlet) {
+        servlet.setThreadContextInheritable(true);
+        return new ServletRegistrationBean<>(servlet, "/**");
+    }
+
+    /**
+     * 覆写拦截器，在feign发送请求前取出原来的header并转发
+     *
+     * @return 拦截器
+     */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return (template) -> {
+            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+            if (requestAttributes != null) {
+                ServletRequestAttributes attributes = (ServletRequestAttributes) requestAttributes;
+                HttpServletRequest request = attributes.getRequest();
+                //获取请求头
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        String name = headerNames.nextElement();
+                        String values = request.getHeader(name);
+                        //将请求头保存到模板中
+                        template.header(name, values);
+                    }
+                }
+            }
+        };
+    }
+
+
+    @Bean
+    Logger.Level feignLogger(){
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public Decoder feignDecoder() {
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new ResponseEntityDecoder(new SpringDecoder(objectFactory));
+    }
+
+
+    @Bean
+    public Encoder feignEncoder(){
+        HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter(objectMapper());
+        ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
+        return new SpringEncoder(objectFactory);
+    }
+
+
+    public ObjectMapper objectMapper(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        return objectMapper;
+    }
+
+
+
+}
+
